626ca0503f291f7964ddb023590e590ee0225c79
Kevin Coffey <kevin.coffey@datadoghq.com>
2020-01-10 11:17:51 -0500

    Small dots, better names for missing auto attacks

--- src/components/Teatime/modules/EnemyAttacks.tsx
+++ src/components/Teatime/modules/EnemyAttacks.tsx
@@ -9,12 +9,15 @@ import Timeline, {Item, ItemGroup} from 'parser/core/modules/Timeline'
 // type: 'cast'
 // ability.guid: 18808
 
+// TODO: Do something with the per player damage events
+
 export default class EnemyAttacks extends Module {
 	static handle = 'enemyAttacks'
 
 	@dependency private timeline!: Timeline
 
 	private damageEvents: DamageEvent[] = []
+	private targetDamageEvents: {[key: number]: DamageEvent[]} = {}
 
 	protected init() {
 		this.addHook('damage', {sourceIsFriendly: false}, this.onDamage)
@@ -24,31 +27,32 @@ export default class EnemyAttacks extends Module {
 
 	private onDamage(event: DamageEvent) {
 		this.damageEvents.push(event)
+		if (!_.isNil(event.targetID)) {
+			if (_.isNil(this.targetDamageEvents[event.targetID])) {
+				this.targetDamageEvents[event.targetID] = []
+			}
+			this.targetDamageEvents[event.targetID].push(event)
+		}
 	}
 
 	private _onComplete() {
 		const startTime = this.parser.fight.start_time
 
-		// TODO: Add them to the group of the player?
-		// const group = new ItemGroup({
-		// 	id: -99,
-		// 	content: 'Boss Damage',
-		// 	visible: true,
-		// })
-		// this.timeline.addGroup(group)
+		// TODO: This is TEA specific data
 		const ABILITY_NAMES: {[key: number]: string} = {
 			18808 : 'Auto Attack', // LL
 			18809 : 'Auto Attack', // HAND
+			19278 : 'Auto Attack', // Jagd Doll
 		}
-		const halfSecond = 500
+
 		for (const event of this.damageEvents) {
 			const abilityName = event.ability.name || ABILITY_NAMES[event.ability.guid] || `${event.ability.guid}`
 			this.timeline.addItem(new Item({
 				type: 'point',
 				group: event.targetID,
-				style: 'background-color: #ce909085;',
+				// style: 'background-color: #ce909085;',
 				start: event.timestamp - startTime,
-				end: event.timestamp - startTime + halfSecond,
+				// end: event.timestamp - startTime,
 				title: abilityName,
 			}))
 		}
